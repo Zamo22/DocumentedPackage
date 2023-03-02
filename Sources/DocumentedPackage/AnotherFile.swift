c56258adb92efc39caddc0ee1f1e20729753fd82
Zaheer <zaheer-dvt@paystack.com>
2023-03-02 16:58:29 +0200

    Added some files without building documentation to see what happens

---
+++ Sources/DocumentedPackage/AnotherFile.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Zaheer Moola on 2023/03/02.
+//
+
+import Foundation
+
+/// This is yet another file
+public struct AnotherFile {
+
+    /// Performs a very complicated mathematical equation
+    /// - Parameters:
+    ///   - number1: Any number you like
+    ///   - number2: Another number you like, even the same one
+    /// - Returns: A sum of the two!
+    public func add(number1: Int, number2: Int) -> Int {
+        number1 + number2
+    }
+}
---
+++ Sources/DocumentedPackage/InternalStruct.swift
@@ -0,0 +1,13 @@
+//
+//  File 2.swift
+//  
+//
+//  Created by Zaheer Moola on 2023/03/02.
+//
+
+import Foundation
+
+/// This is an internal struct, does it appear?
+struct InternalStruct {
+    var a = "A"
+}
